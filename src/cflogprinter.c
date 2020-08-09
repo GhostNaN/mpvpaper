87249b7166564b00bc556a8835db02969203e286
jdbm8992 <daniel@breedveld.pw>
2020-08-09 21:03:56 +0200

    added cflogprinter

---
+++ src/cflogprinter.c
@@ -0,0 +1,79 @@
+#include <cflogprinter.h>
+
+void cflp_good(char *msg, ...) {
+    printf("%s[+] ", GREEN);
+
+    va_list arg;
+
+    va_start (arg, msg);
+    vfprintf (stdout, msg, arg);
+    va_end (arg);
+
+    printf("%s\n", RESET);
+
+}
+
+void cflp_bad(char *msg, ...) {
+    printf("%s[-] ", RED);
+
+    va_list arg;
+
+    va_start (arg, msg);
+    vfprintf (stdout, msg, arg);
+    va_end (arg);
+
+    printf("%s\n", RESET);
+
+}
+
+void cflp_bad_error(char *msg, ...) {
+    printf("%s[-] ", RED);
+
+    va_list arg;
+
+    va_start (arg, msg);
+    vfprintf (stderr, msg, arg); //print to stderr instead of stdout!
+    va_end (arg);
+
+    printf("%s\n", RESET);
+
+}
+
+void cflp_warning(char *msg, ...) {
+    printf("%s[!] ", RED);
+
+    va_list arg;
+
+    va_start (arg, msg);
+    vfprintf (stdout, msg, arg);
+    va_end (arg);
+
+    printf("%s\n", RESET);
+
+}
+
+void cflp_info(char *msg, ...) {
+    printf("[*] ");
+
+    va_list arg;
+
+    va_start (arg, msg);
+    vfprintf (stdout, msg, arg);
+    va_end (arg);
+
+    printf("%s\n", RESET);
+
+}
+
+void cflp_custom(char *color, char *msg, ...) {
+    printf("%s", color);
+
+    va_list arg;
+
+    va_start (arg, msg);
+    vfprintf (stdout, msg, arg);
+    va_end (arg);
+
+    printf("%s\n", RESET);
+
+}
